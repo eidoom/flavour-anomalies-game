bac8801232b51413409d6d2b817834edcbf0fb60
ryan <ryanmoodie@gmail.com>
2019-01-18 19:27:34 +0000

    Added win condition

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 from random import choice, randint
 
 EMPTY = " "
-OBSTACLE = "0"
+OBSTACLES = ["T", "O"]
+END = "A"
 PLAYER = "X"
 HORIZONTAL_BORDER = "-"
 VERTICAL_BORDER = "|"
@@ -28,7 +29,7 @@ def main():
     print("Use wasd controls")
 
     def tiles(empty_ratio):
-        return [EMPTY] * empty_ratio + [OBSTACLE]
+        return [EMPTY] * empty_ratio + OBSTACLES
 
     # def empty_world_matrix(length):
     #     return [EMPTY * length] * length
@@ -38,7 +39,7 @@ def main():
         return [[choice(tiles(empty_ratio)) for _ in range(j_length)] for _ in range(i_length)]
 
     def render_world(data):
-        border = HORIZONTAL_BORDER*(len(data[0])+2) + "\n"
+        border = HORIZONTAL_BORDER * (len(data[0]) + 2) + "\n"
         return "".join([border,
                         *["".join([VERTICAL_BORDER, *[a for a in line], VERTICAL_BORDER + "\n"]) for line in data],
                         border])
@@ -47,10 +48,10 @@ def main():
         i_length, j_length = limits
         return randint(0, i_length - 1), randint(0, j_length - 1)
 
-    def generate_start_position(data, limits):
+    def generate_allowed_position(data, limits):
         i, j = generate_random_position(limits)
-        if data[i][j] == OBSTACLE:
-            generate_start_position(data, limits)
+        if data[i][j] in OBSTACLES:
+            generate_allowed_position(data, limits)
         return i, j
 
     def move(data, position):
@@ -67,23 +68,34 @@ def main():
         else:
             print("Use wasd!")
             return move(data, position)
-        if (i + 1 > len(list(zip(*data))[0])) or (j + 1 > len(data[0])) or data[i][j] is not EMPTY:
+        if (i + 1 > len(list(zip(*data))[0])) or (j + 1 > len(data[0])) or data[i][j] in OBSTACLES:
             print("You can't do that!")
             return move(data, position)
         return i, j
 
-    def game(data, position):
+    def turn(data, position, end):
         i, j = position
         data[i][j] = PLAYER
         print(render_world(data))
+        if position == end:
+            print("You won!")
+            exit()
         data[i][j] = EMPTY
         new_position = move(data, position)
-        game(data, new_position)
+        turn(data, new_position, end)
 
-    sides = 50, 200
-    world_data = world_matrix(sides, 15)
-    start = generate_start_position(world_data, sides)
-    game(world_data, start)
+    def game(i_length, j_length, emptiness):
+        sides = i_length, j_length
+        world_data = world_matrix(sides, emptiness)
+        start = generate_allowed_position(world_data, sides)
+        end = generate_allowed_position(world_data, sides)
+        i, j = end
+        world_data[i][j] = END
+        turn(world_data, start, end)
+
+    # game(50, 200, 30)
+    # game(25, 100, 30)
+    game(12, 50, 30)
 
 
 if __name__ == "__main__":
